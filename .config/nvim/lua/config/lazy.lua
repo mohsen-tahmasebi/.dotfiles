67d0eaed2f4d7c9597a7c24f6e1f7cb508844ddd
Mohsen Tahmasebi <redboy5093@gmail.com>
2024-09-03 10:41:41 +0330

    improve, optimize and update nvim

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -19,6 +19,7 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import/override with your plugins
+
     { import = "plugins" },
   },
   defaults = {
@@ -47,4 +48,16 @@ require("lazy").setup({
       },
     },
   },
+})
+
+require("nvim-ts-autotag").setup({
+  opts = {
+    -- Defaults
+    enable_close = true, -- Auto close tags
+    enable_rename = true, -- Auto rename pairs of tags
+    enable_close_on_slash = false, -- Auto close on trailing </
+  },
+  -- Also override individual filetype configs, these take priority.
+  -- Empty by default, useful if one of the "opts" global settings
+  -- doesn't work well in a specific filetype
 })
