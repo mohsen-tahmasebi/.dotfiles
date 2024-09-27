5b06376413cc64f0edb0a33e9f7411165ea58399
Mohsen Tahmasebi <redboy5093@gmail.com>
2024-09-27 20:58:55 +0330

    add nvim-surround

---
+++ .config/nvim/lua/plugins/nvim-surround.lua
@@ -0,0 +1,23 @@
+return {
+  "kylechui/nvim-surround",
+  version = "*", -- Use for stability; omit to use `main` branch for the latest features
+  event = "VeryLazy",
+  config = function()
+    require("nvim-surround").setup({
+      -- Configuration here, or leave empty to use defaults
+      keymaps = {
+        insert = "<C-g>s",
+        insert_line = "<C-g>S",
+        normal = "gs",
+        normal_cur = "gss",
+        normal_line = "gS",
+        normal_cur_line = "gSS",
+        visual = "S",
+        visual_line = "gS",
+        delete = "gsd",
+        change = "gsc",
+        change_line = "cS",
+      },
+    })
+  end,
+}
